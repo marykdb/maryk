b646b8905840245b0b1a5c1ab9c092ffcfd42cda
Jurriaan Mous <jurmous@jurmo.us>
2017-12-20 17:02:31 +0100

    Add missing param to DataModel documentation. First documentation in recommended JetBrains style.

--- src/main/kotlin/maryk/core/objects/DataModel.kt
+++ src/main/kotlin/maryk/core/objects/DataModel.kt
@@ -4,10 +4,9 @@ import maryk.core.properties.definitions.PropertyDefinitions
 import maryk.core.properties.definitions.wrapper.IsPropertyDefinitionWrapper
 import maryk.core.properties.types.TypedValue
 
-/** DataModel for non contextual models
- * @param properties: All definitions for properties contained in this model
- * @param DO: Type of DataObject contained
- * @param P: PropertyDefinitions type for reference retrieval
+/** DataModel for non contextual models. Contains a [name] to identify the model and [properties] which define how the
+ * properties should be validated. It models the DataObjects of type [DO] which can be validated. And it contains a
+ * reference to the propertyDefinitions of type [P] which can be used for the references to the properties.
  */
 abstract class DataModel<DO: Any, out P: PropertyDefinitions<DO>>(
         val name: String,

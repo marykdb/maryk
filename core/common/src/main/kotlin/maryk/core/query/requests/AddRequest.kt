e252bafb1a23f7d773bab5c30ff722c45809ee69
Jurriaan Mous <jurmous@jurmo.us>
2018-10-22 21:45:14 +0200

    Make Properties strongly typed in AddRequest. This way Values can be processed in a type strict way.

--- core/common/src/main/kotlin/maryk/core/query/requests/AddRequest.kt
+++ core/common/src/main/kotlin/maryk/core/query/requests/AddRequest.kt
@@ -16,19 +16,19 @@ import maryk.core.query.RequestContext
 import maryk.core.query.responses.AddResponse
 
 /** Creates a Request to add multiple [objectToAdd] to a store defined by given DataModel */
-fun <DM: IsRootValuesDataModel<*>> DM.add(vararg objectToAdd: Values<DM, *>) =
+fun <DM: IsRootValuesDataModel<P>, P: PropertyDefinitions> DM.add(vararg objectToAdd: Values<DM, P>) =
     AddRequest(this, objectToAdd.toList())
 
 /** A Request to add [objectsToAdd] to [dataModel] */
-data class AddRequest<DM: IsRootValuesDataModel<*>> internal constructor(
+data class AddRequest<DM: IsRootValuesDataModel<P>, P: PropertyDefinitions> internal constructor(
     override val dataModel: DM,
-    val objectsToAdd: List<Values<DM, *>>
+    val objectsToAdd: List<Values<DM, P>>
 ) : IsObjectRequest<DM, AddResponse<*>> {
     override val requestType = RequestType.Add
     override val responseModel = AddResponse
 
-    object Properties : ObjectPropertyDefinitions<AddRequest<*>>() {
-        val dataModel = IsObjectRequest.addDataModel(this, AddRequest<*>::dataModel)
+    object Properties : ObjectPropertyDefinitions<AddRequest<*, *>>() {
+        val dataModel = IsObjectRequest.addDataModel(this, AddRequest<*, *>::dataModel)
 
         @Suppress("UNCHECKED_CAST", "unused")
         val objectsToAdd = add(2, "objectsToAdd", ListDefinition(
@@ -38,13 +38,13 @@ data class AddRequest<DM: IsRootValuesDataModel<*>> internal constructor(
                     it?.dataModel as? ValuesDataModelImpl<RequestContext>? ?: throw ContextNotFoundException()
                 }
             ) as IsValueDefinition<Values<out IsValuesDataModel<*>, out PropertyDefinitions>, RequestContext>
-        ), AddRequest<*>::objectsToAdd)
+        ), AddRequest<*, *>::objectsToAdd)
     }
 
-    companion object: QueryDataModel<AddRequest<*>, Properties>(
+    companion object: QueryDataModel<AddRequest<*, *>, Properties>(
         properties = Properties
     ) {
-        override fun invoke(map: ObjectValues<AddRequest<*>, Properties>) = AddRequest(
+        override fun invoke(map: ObjectValues<AddRequest<*, *>, Properties>) = AddRequest<IsRootValuesDataModel<PropertyDefinitions>, PropertyDefinitions>(
             dataModel = map(1),
             objectsToAdd = map(2)
         )

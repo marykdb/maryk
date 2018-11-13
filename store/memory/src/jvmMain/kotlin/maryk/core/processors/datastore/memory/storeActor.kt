be47b4d9512572e119b544f8adf79f031e41c1b5
Jurriaan Mous <jurmous@jurmo.us>
2018-11-13 23:20:56 +0100

    StoreActor for JVM to LinkedList

--- store/memory/src/jvmMain/kotlin/maryk/core/processors/datastore/memory/storeActor.kt
+++ store/memory/src/jvmMain/kotlin/maryk/core/processors/datastore/memory/storeActor.kt
@@ -7,9 +7,10 @@ import kotlinx.coroutines.channels.actor
 import maryk.core.models.IsRootValuesDataModel
 import maryk.core.processors.datastore.memory.records.DataRecord
 import maryk.core.properties.PropertyDefinitions
+import java.util.LinkedList
 
 internal actual fun <DM: IsRootValuesDataModel<P>, P: PropertyDefinitions> CoroutineScope.storeActor(store: InMemoryDataStore, executor: StoreExecutor<DM, P>): StoreActor<DM, P> = actor {
-    val listOfData = mutableListOf<DataRecord<DM, P>>()
+    val listOfData = LinkedList<DataRecord<DM, P>>()
 
     for (msg in channel) { // iterate over incoming messages
         executor(Unit, store, msg, listOfData)

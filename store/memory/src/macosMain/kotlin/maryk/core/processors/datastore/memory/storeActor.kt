7a83465ae5259a47a8a906dc44074458b1ef573c
Jurriaan Mous <jurmous@jurmo.us>
2018-11-14 10:05:25 +0100

    Fix native

--- store/memory/src/macosMain/kotlin/maryk/core/processors/datastore/memory/storeActor.kt
+++ store/memory/src/macosMain/kotlin/maryk/core/processors/datastore/memory/storeActor.kt
@@ -4,6 +4,6 @@ import kotlinx.coroutines.CoroutineScope
 import maryk.core.models.IsRootValuesDataModel
 import maryk.core.properties.PropertyDefinitions
 
-internal actual fun <DM: IsRootValuesDataModel<P>, P: PropertyDefinitions> CoroutineScope.storeActor(store: InMemoryDataStore, executor: StoreExecutor<DM, P>): StoreActor<DM, P> = actor {
+internal actual fun <DM: IsRootValuesDataModel<P>, P: PropertyDefinitions> CoroutineScope.storeActor(store: InMemoryDataStore, executor: StoreExecutor<DM, P>): StoreActor<DM, P> {
     TODO()
 }

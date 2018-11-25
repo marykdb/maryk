fd66ea1dce9e9e97b8da908a05fdd317b8d40eff
Jurriaan Mous <jurmous@jurmo.us>
2018-11-25 18:03:21 +0100

    Add filtering and tests to Memory datastore

--- store/memory/src/commonMain/kotlin/maryk/datastore/memory/processors/filterWithFetchRequest.kt
+++ store/memory/src/commonMain/kotlin/maryk/datastore/memory/processors/filterWithFetchRequest.kt
@@ -2,6 +2,22 @@ package maryk.datastore.memory.processors
 
 import maryk.core.models.IsRootValuesDataModel
 import maryk.core.properties.PropertyDefinitions
+import maryk.core.query.ValueRange
+import maryk.core.query.filters.And
+import maryk.core.query.filters.Equals
+import maryk.core.query.filters.Exists
+import maryk.core.query.filters.FilterType
+import maryk.core.query.filters.GreaterThan
+import maryk.core.query.filters.GreaterThanEquals
+import maryk.core.query.filters.IsFilter
+import maryk.core.query.filters.LessThan
+import maryk.core.query.filters.LessThanEquals
+import maryk.core.query.filters.Not
+import maryk.core.query.filters.Or
+import maryk.core.query.filters.Prefix
+import maryk.core.query.filters.Range
+import maryk.core.query.filters.RegEx
+import maryk.core.query.filters.ValueIn
 import maryk.core.query.requests.IsFetchRequest
 import maryk.datastore.memory.records.DataRecord
 import maryk.datastore.memory.records.DeleteState.Deleted
@@ -10,5 +26,124 @@ internal fun <DM: IsRootValuesDataModel<P>, P: PropertyDefinitions> IsFetchReque
     dataRecord: DataRecord<DM, P>
 ) = when {
     this.filterSoftDeleted && dataRecord.isDeleted is Deleted -> true
+    this.filter != null -> doFilter(this.filter as IsFilter, dataRecord)
     else -> false
+}
+
+internal fun <DM: IsRootValuesDataModel<P>, P: PropertyDefinitions> doFilter(filter: IsFilter, dataRecord: DataRecord<DM, P>): Boolean {
+    when(filter.filterType) {
+        FilterType.And -> {
+            val and = filter as And
+            for (f in and.filters) {
+                if(!doFilter(f, dataRecord)) return false
+            }
+            return true
+        }
+        FilterType.Or -> {
+            val or = filter as Or
+            for (f in or.filters) {
+                if(doFilter(f, dataRecord)) return true
+            }
+            return false
+        }
+        FilterType.Not -> {
+            val notFilter = (filter as Not)
+            for (aFilter in notFilter.filters) {
+                // If internal filter succeeds, then fail
+                if (doFilter(aFilter, dataRecord)) return false
+            }
+            return true
+        }
+        FilterType.Exists -> {
+            val exists = filter as Exists
+            for (propRef in exists.references) {
+                if (dataRecord[propRef] == null) return false
+            }
+            return true
+        }
+        FilterType.Equals -> {
+            val equals = filter as Equals
+            for ((propRef, value) in equals.referenceValuePairs) {
+                if (dataRecord[propRef] != value) return false
+            }
+            return true
+        }
+        FilterType.LessThan -> {
+            val lessThan = filter as LessThan
+            for ((propRef, value) in lessThan.referenceValuePairs) {
+                dataRecord[propRef]?.let {
+                    @Suppress("UNCHECKED_CAST")
+                    if ((value as Comparable<Any>) <= it) return false
+                }
+            }
+            return true
+        }
+        FilterType.LessThanEquals -> {
+            val lessThanEquals = filter as LessThanEquals
+            for ((propRef, value) in lessThanEquals.referenceValuePairs) {
+                dataRecord[propRef]?.let {
+                    @Suppress("UNCHECKED_CAST")
+                    if ((value as Comparable<Any>) < it) return false
+                }
+            }
+            return true
+        }
+        FilterType.GreaterThan -> {
+            val greaterThan = filter as GreaterThan
+            for ((propRef, value) in greaterThan.referenceValuePairs) {
+                dataRecord[propRef]?.let {
+                    @Suppress("UNCHECKED_CAST")
+                    if ((value as Comparable<Any>) >= it) return false
+                }
+            }
+            return true
+        }
+        FilterType.GreaterThanEquals -> {
+            val greaterThanEquals = filter as GreaterThanEquals
+            for ((propRef, value) in greaterThanEquals.referenceValuePairs) {
+                dataRecord[propRef]?.let {
+                    @Suppress("UNCHECKED_CAST")
+                    if ((value as Comparable<Any>) > it) return false
+                }
+            }
+            return true
+        }
+        FilterType.Prefix -> {
+            val prefixFilter = filter as Prefix
+            for ((propRef, prefix) in prefixFilter.referenceValuePairs) {
+                dataRecord[propRef]?.let {
+                    if(!(it).startsWith(prefix)) return false
+                }
+            }
+            return true
+        }
+        FilterType.Range -> {
+            val rangeFilter = filter as Range
+            for ((propRef, range) in rangeFilter.referenceRangePairs) {
+                dataRecord[propRef]?.let {
+                    @Suppress("UNCHECKED_CAST")
+                    if ((it as Comparable<Any>) !in range as ValueRange<Comparable<Any>>) return false
+                }
+            }
+            return true
+        }
+        FilterType.RegEx -> {
+            val regExFilter = filter as RegEx
+            for ((propRef, regEx) in regExFilter.referenceValuePairs) {
+                dataRecord[propRef]?.let {
+                    if(!Regex(regEx).matches(it)) return false
+                }
+            }
+            return true
+        }
+        FilterType.ValueIn -> {
+            val valueInFilter = filter as ValueIn
+            for ((propRef, values) in valueInFilter.referenceValuePairs) {
+                dataRecord[propRef]?.let {
+                    if(!values.contains(it)) return false
+                }
+            }
+            return true
+        }
+    }
 }
---
+++ store/memory/src/commonTest/kotlin/maryk/datastore/memory/processors/FilterWithFetchRequestKtTest.kt
@@ -0,0 +1,264 @@
+@file:Suppress("EXPERIMENTAL_API_USAGE", "EXPERIMENTAL_UNSIGNED_LITERALS")
+
+package maryk.datastore.memory.processors
+
+import maryk.core.models.IsRootValuesDataModel
+import maryk.core.models.key
+import maryk.core.processors.datastore.walkForStorage
+import maryk.core.properties.PropertyDefinitions
+import maryk.core.query.filters.And
+import maryk.core.query.filters.Equals
+import maryk.core.query.filters.Exists
+import maryk.core.query.filters.GreaterThan
+import maryk.core.query.filters.GreaterThanEquals
+import maryk.core.query.filters.LessThan
+import maryk.core.query.filters.LessThanEquals
+import maryk.core.query.filters.Not
+import maryk.core.query.filters.Or
+import maryk.core.query.filters.Prefix
+import maryk.core.query.filters.Range
+import maryk.core.query.filters.RegEx
+import maryk.core.query.filters.ValueIn
+import maryk.core.query.pairs.with
+import maryk.core.values.Values
+import maryk.datastore.memory.records.DataRecord
+import maryk.datastore.memory.records.DataRecordValue
+import maryk.lib.time.DateTime
+import maryk.test.models.TestMarykModel
+import maryk.test.shouldBe
+import kotlin.test.Test
+
+class FilterWithFetchRequestKtTest {
+    private val value1 = TestMarykModel.createDataRecord(
+        TestMarykModel("haha1", 5, 6u, 0.43, DateTime(2018, 3, 2), true)
+    )
+
+    private fun <DM: IsRootValuesDataModel<P>, P: PropertyDefinitions> DM.createDataRecord(values: Values<DM, P>): DataRecord<DM, P> {
+        val recordValues = mutableListOf<DataRecordValue<*>>()
+
+        values.walkForStorage { _, reference, _, value ->
+            recordValues += DataRecordValue(reference, value, 1234uL)
+        }
+
+        return DataRecord(
+            key = this.key(values),
+            firstVersion = 1234uL,
+            lastVersion = 1234uL,
+            values = recordValues
+        )
+    }
+
+    @Test
+    fun doExistsFilter() {
+        doFilter(
+            Exists(TestMarykModel.ref { string }),
+            value1
+        ) shouldBe true
+
+        doFilter(
+            Exists(TestMarykModel.ref { reference }),
+            value1
+        ) shouldBe false
+    }
+
+    @Test
+    fun doEqualsFilter() {
+        doFilter(
+            Equals(TestMarykModel.ref { string } with "haha1"),
+            value1
+        ) shouldBe true
+
+        doFilter(
+            Equals(TestMarykModel.ref { string } with "wrong"),
+            value1
+        ) shouldBe false
+    }
+
+    @Test
+    fun doPrefixFilter() {
+        doFilter(
+            Prefix(TestMarykModel.ref { string } with "ha"),
+            value1
+        ) shouldBe true
+
+        doFilter(
+            Prefix(TestMarykModel.ref { string } with "wrong"),
+            value1
+        ) shouldBe false
+    }
+
+    @Test
+    fun doLessThanFilter() {
+        doFilter(
+            LessThan(TestMarykModel.ref { int } with 6),
+            value1
+        ) shouldBe true
+
+        doFilter(
+            LessThan(TestMarykModel.ref { int } with 5),
+            value1
+        ) shouldBe false
+
+        doFilter(
+            LessThan(TestMarykModel.ref { int } with 2),
+            value1
+        ) shouldBe false
+    }
+
+    @Test
+    fun doLessThanEqualsFilter() {
+        doFilter(
+            LessThanEquals(TestMarykModel.ref { int } with 6),
+            value1
+        ) shouldBe true
+
+        doFilter(
+            LessThanEquals(TestMarykModel.ref { int } with 5),
+            value1
+        ) shouldBe true
+
+        doFilter(
+            LessThanEquals(TestMarykModel.ref { int } with 2),
+            value1
+        ) shouldBe false
+    }
+
+    @Test
+    fun doGreaterThanFilter() {
+        doFilter(
+            GreaterThan(TestMarykModel.ref { int } with 4),
+            value1
+        ) shouldBe true
+
+        doFilter(
+            GreaterThan(TestMarykModel.ref { int } with 5),
+            value1
+        ) shouldBe false
+
+        doFilter(
+            GreaterThan(TestMarykModel.ref { int } with 6),
+            value1
+        ) shouldBe false
+    }
+
+    @Test
+    fun doGreaterThanEqualsFilter() {
+        doFilter(
+            GreaterThanEquals(TestMarykModel.ref { int } with 4),
+            value1
+        ) shouldBe true
+
+        doFilter(
+            GreaterThanEquals(TestMarykModel.ref { int } with 5),
+            value1
+        ) shouldBe true
+
+        doFilter(
+            GreaterThanEquals(TestMarykModel.ref { int } with 6),
+            value1
+        ) shouldBe false
+    }
+
+    @Test
+    fun doRangeFilter() {
+        doFilter(
+            Range(TestMarykModel.ref { int } with (2..8)),
+            value1
+        ) shouldBe true
+
+        doFilter(
+            Range(TestMarykModel.ref { int } with (2..5)),
+            value1
+        ) shouldBe true
+
+        doFilter(
+            Range(TestMarykModel.ref { int } with (2..3)),
+            value1
+        ) shouldBe false
+    }
+
+    @Test
+    fun doRegExFilter() {
+        doFilter(
+            RegEx(TestMarykModel.ref { string } with "^h.*$"),
+            value1
+        ) shouldBe true
+
+        doFilter(
+            RegEx(TestMarykModel.ref { string } with "^b.*$"),
+            value1
+        ) shouldBe false
+    }
+
+    @Test
+    fun doValueInFilter() {
+        doFilter(
+            ValueIn(TestMarykModel.ref { string } with setOf("haha1", "haha2")),
+            value1
+        ) shouldBe true
+
+        doFilter(
+            ValueIn(TestMarykModel.ref { string } with setOf("no1", "no2")),
+            value1
+        ) shouldBe false
+    }
+
+    @Test
+    fun doNotFilter() {
+        doFilter(
+            Not(Exists(TestMarykModel.ref { string })),
+            value1
+        ) shouldBe false
+
+        doFilter(
+            Not(Exists(TestMarykModel.ref { reference })),
+            value1
+        ) shouldBe true
+    }
+
+    @Test
+    fun doAndFilter() {
+        doFilter(
+            And(
+                Exists(TestMarykModel.ref { int }),
+                Exists(TestMarykModel.ref { string })
+            ),
+            value1
+        ) shouldBe true
+
+        doFilter(
+            And(
+                Exists(TestMarykModel.ref { reference }),
+                Exists(TestMarykModel.ref { string })
+            ),
+            value1
+        ) shouldBe false
+    }
+
+    @Test
+    fun doOrFilter() {
+        doFilter(
+            Or(
+                Exists(TestMarykModel.ref { int }),
+                Exists(TestMarykModel.ref { string })
+            ),
+            value1
+        ) shouldBe true
+
+        doFilter(
+            Or(
+                Exists(TestMarykModel.ref { reference }),
+                Exists(TestMarykModel.ref { string })
+            ),
+            value1
+        ) shouldBe true
+
+        doFilter(
+            Or(
+                Exists(TestMarykModel.ref { reference }),
+                Not(Exists(TestMarykModel.ref { string }))
+            ),
+            value1
+        ) shouldBe false
+    }
+}

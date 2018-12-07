5601839b4493f72939cf5f1fb338b018d3458472
Jurriaan Mous <jurmous@jurmo.us>
2018-12-07 22:06:54 +0100

    Fix KN tests in core

--- core/src/commonTest/kotlin/maryk/core/processors/datastore/ValuesToTest.kt
+++ core/src/commonTest/kotlin/maryk/core/processors/datastore/ValuesToTest.kt
@@ -6,6 +6,7 @@ import maryk.lib.time.Date
 import maryk.lib.time.DateTime
 import maryk.lib.time.Time
 import maryk.test.models.EmbeddedMarykModel
+import maryk.test.models.Option.V1
 import maryk.test.models.TestMarykModel
 
 val testMaryk = TestMarykModel(
@@ -35,12 +36,12 @@ val testMaryk = TestMarykModel(
 )
 
 val valuesAsStorables = arrayOf<Pair<String, Any>>(
-    "09" to testMaryk { string }!!,
-    "11" to testMaryk { int }!!,
-    "19" to testMaryk { uint }!!,
-    "21" to testMaryk { double }!!,
-    "29" to testMaryk { dateTime }!!,
-    "39" to testMaryk { enum }!!,
+    "09" to "hello world",
+    "11" to 5,
+    "19" to 3u,
+    "21" to 2.3,
+    "29" to DateTime(2018, 7, 18),
+    "39" to V1,
     "4b" to 3,
     "4b80004577" to Date(2018, 9, 9),
     "4b80001104" to Date(1981, 12, 5),
@@ -48,8 +49,8 @@ val valuesAsStorables = arrayOf<Pair<String, Any>>(
     "54" to 2,
     "54009ff9" to "eleven",
     "5400ae46" to "twelve",
-    "6109" to testMaryk { embeddedValues }!!{ value }!!,
-    "611109" to testMaryk { embeddedValues }!!{ model }!!{ value }!!,
+    "6109" to "test",
+    "611109" to "another test",
     "7a" to 3,
     "7a00000000" to "v1",
     "7a00000001" to "v2",

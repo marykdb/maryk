13c08450820d95aa677e08be29fec90db4c40fd8
Jurriaan Mous <jurmous@jurmo.us>
2018-12-22 10:59:40 +0100

    Unsupport JS and Native for Store Memory until we have proper actor support

--- store/memory/src/jsMain/kotlin/maryk/datastore/memory/storeActor.kt
+++
@@ -1,9 +0,0 @@
-package maryk.datastore.memory
-
-import kotlinx.coroutines.CoroutineScope
-import maryk.core.models.IsRootValuesDataModel
-import maryk.core.properties.PropertyDefinitions
-
-internal actual fun <DM: IsRootValuesDataModel<P>, P: PropertyDefinitions> CoroutineScope.storeActor(store: InMemoryDataStore, executor: StoreExecutor<DM, P>): StoreActor<DM, P> {
-    TODO()
-}

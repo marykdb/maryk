a5f5691850a3e897e76f162fb836084bedda6cb1
Jurriaan Mous <jurmous@jurmo.us>
2019-01-09 14:48:24 +0100

    Add multi to ComplexDataModelTest

--- core/src/commonTest/kotlin/maryk/core/models/ComplexDataModelTest.kt
+++ core/src/commonTest/kotlin/maryk/core/models/ComplexDataModelTest.kt
@@ -16,6 +16,7 @@ import maryk.yaml.YamlWriter
 import kotlin.test.Test
 
 val testComplexMap = ComplexModel(
+    multi = TypedValue(V3, EmbeddedMarykModel("u3", EmbeddedMarykModel("ue3"))),
     mapStringString = mapOf("v1" to "a", "v2" to "b"),
     mapIntObject = mapOf(1u to EmbeddedMarykModel("t1"), 2u to EmbeddedMarykModel("t2")),
     mapIntMulti = mapOf(2u to TypedValue(V3, EmbeddedMarykModel("m3")))
@@ -33,6 +34,12 @@ internal class ComplexDataModelMapTest {
 
         output shouldBe """
         {
+        	"multi": ["V3", {
+        		"value": "u3",
+        		"model": {
+        			"value": "ue3"
+        		}
+        	}],
         	"mapStringString": {
         		"v1": "a",
         		"v2": "b"
@@ -68,6 +75,10 @@ internal class ComplexDataModelMapTest {
         ComplexModel.writeJson(testComplexMap, writer)
 
         output shouldBe """
+        multi: !V3
+          value: u3
+          model:
+            value: ue3
         mapStringString:
           v1: a
           v2: b
@@ -94,7 +105,7 @@ internal class ComplexDataModelMapTest {
 
         ComplexModel.writeProtoBuf(testComplexMap, cache, bc::write)
 
-        bc.bytes!!.toHex() shouldBe "12070a02763112016112070a0276321201621a08080112040a0274311a08080212040a027432220a080212061a040a026d33"
+        bc.bytes!!.toHex() shouldBe "0a0d1a0b0a02753312050a0375653312070a02763112016112070a0276321201621a08080112040a0274311a08080212040a027432220a080212061a040a026d33"
 
         ComplexModel.readProtoBuf(bc.size, bc::read) shouldBe testComplexMap
     }

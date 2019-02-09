f17a7284b77dd14e9e5bd49768d42054cb15a276
Jurriaan Mous <jurmous@jurmo.us>
2019-02-09 16:10:12 +0100

    Update test-lib dependencies to use simpler kotlin gradle notation

--- testlib/build.gradle.kts
+++ testlib/build.gradle.kts
@@ -22,27 +22,27 @@ kotlin {
     sourceSets {
         commonMain {
             dependencies {
-                api("org.jetbrains.kotlin:kotlin-stdlib-common")
-                api("org.jetbrains.kotlin:kotlin-test-common")
-                api("org.jetbrains.kotlin:kotlin-test-annotations-common")
+                api(kotlin("stdlib-common"))
+                api(kotlin("test-common"))
+                api(kotlin("test-annotations-common"))
 
                 api("org.jetbrains.kotlinx:kotlinx-coroutines-core-common:$coroutinesVersion")
             }
         }
         jvm().compilations["main"].defaultSourceSet {
             dependencies {
-                api("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-                api("org.jetbrains.kotlin:kotlin-test")
-                api("org.jetbrains.kotlin:kotlin-test-junit")
-                implementation("org.jetbrains.kotlin:kotlin-reflect")
+                api(kotlin("stdlib-jdk8"))
+                api(kotlin("test"))
+                api(kotlin("test-junit"))
+                implementation(kotlin("reflect"))
 
                 api("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:$coroutinesVersion")
             }
         }
         js().compilations["main"].defaultSourceSet {
             dependencies {
-                api("org.jetbrains.kotlin:kotlin-stdlib-js")
-                api("org.jetbrains.kotlin:kotlin-test-js")
+                api(kotlin("stdlib-js"))
+                api(kotlin("test-js"))
 
                 api("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:$coroutinesVersion")
             }

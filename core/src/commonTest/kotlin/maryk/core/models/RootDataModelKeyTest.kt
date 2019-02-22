5d3e007ac4e9ff323430f197b5f6b712e3602584
Jurriaan Mous <jurmous@jurmo.us>
2019-02-22 21:08:55 +0100

    Add test for Flex encoded definitions not to be used for keys

--- core/src/commonTest/kotlin/maryk/core/models/RootDataModelKeyTest.kt
+++ core/src/commonTest/kotlin/maryk/core/models/RootDataModelKeyTest.kt
@@ -2,9 +2,11 @@ package maryk.core.models
 
 import maryk.core.models.WrongProperties.boolean
 import maryk.core.models.WrongProperties.dateTime
+import maryk.core.models.WrongProperties.string
 import maryk.core.properties.PropertyDefinitions
 import maryk.core.properties.definitions.BooleanDefinition
 import maryk.core.properties.definitions.DateTimeDefinition
+import maryk.core.properties.definitions.StringDefinition
 import maryk.core.properties.definitions.index.Multiple
 import maryk.lib.time.DateTime
 import maryk.test.shouldThrow
@@ -20,11 +22,14 @@ internal object WrongProperties : PropertyDefinitions() {
     val dateTime = add(
         2, "dateTime", DateTimeDefinition()
     )
+    val string = add(
+        3, "string", StringDefinition()
+    )
 }
 
 class RootDataModelKeyTest {
     @Test
-    fun missingRequiredTest() {
+    fun notAcceptNonRequiredDefinitions() {
         shouldThrow<IllegalArgumentException> {
             object : RootDataModel<IsRootValuesDataModel<WrongProperties>, WrongProperties>(
                 name = "MarykModel",
@@ -39,7 +44,7 @@ class RootDataModelKeyTest {
     }
 
     @Test
-    fun missingFinalTest() {
+    fun notAcceptNonFinalDefinitions() {
         shouldThrow<IllegalArgumentException> {
             object : RootDataModel<IsRootValuesDataModel<WrongProperties>, WrongProperties>(
                 name = "MarykModel",
@@ -50,6 +55,23 @@ class RootDataModelKeyTest {
             ) {
                 operator fun invoke(dateTime: DateTime) = this.values {
                     mapNonNulls(this.dateTime with dateTime)
+                }
+            }
+        }
+    }
+
+    @Test
+    fun notAcceptFlexByteDefinitions() {
+        shouldThrow<Exception> {
+            object : RootDataModel<IsRootValuesDataModel<WrongProperties>, WrongProperties>(
+                name = "MarykModel",
+                keyDefinition = Multiple(
+                    string.ref()
+                ),
+                properties = WrongProperties
+            ) {
+                operator fun invoke(string: String) = this.values {
+                    mapNonNulls(this.string with string)
                 }
             }
         }

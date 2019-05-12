ec14cd9ba577fc652579f3d96708906039f92ec9
Jurriaan Mous <jurmous@jurmo.us>
2019-05-12 12:11:47 +0200

    Remove typing from NumberType since it is only used as Enum

--- core/src/commonMain/kotlin/maryk/core/properties/types/numeric/NumberType.kt
+++ core/src/commonMain/kotlin/maryk/core/properties/types/numeric/NumberType.kt
@@ -4,7 +4,6 @@ import maryk.core.exceptions.DefNotFoundException
 import maryk.core.properties.enum.IndexedEnumDefinition
 import maryk.core.properties.enum.IndexedEnumImpl
 import maryk.core.properties.enum.IsCoreEnum
-import maryk.core.properties.enum.TypeEnum
 
 sealed class NumberType(
     index: UInt,
@@ -12,16 +11,16 @@ sealed class NumberType(
 ) : IndexedEnumImpl<NumberType>(index), IsCoreEnum {
     override val name get() = descriptor()::class.simpleName ?: throw DefNotFoundException("Missing enum option name")
 
-    object SInt8Type : NumberType(1u, { SInt8 }), TypeEnum<Byte>
-    object SInt16Type : NumberType(2u, { SInt16 }), TypeEnum<Short>
-    object SInt32Type : NumberType(3u, { SInt32 }), TypeEnum<Int>
-    object SInt64Type : NumberType(4u, { SInt64 }), TypeEnum<Long>
-    object UInt8Type : NumberType(5u, { UInt8 }), TypeEnum<UByte>
-    object UInt16Type : NumberType(6u, { UInt16 }), TypeEnum<UShort>
-    object UInt32Type : NumberType(7u, { UInt32 }), TypeEnum<UInt>
-    object UInt64Type : NumberType(8u, { UInt64 }), TypeEnum<ULong>
-    object Float32Type : NumberType(9u, { Float32 }), TypeEnum<Float>
-    object Float64Type : NumberType(10u, { Float64 }), TypeEnum<Double>
+    object SInt8Type : NumberType(1u, { SInt8 })
+    object SInt16Type : NumberType(2u, { SInt16 })
+    object SInt32Type : NumberType(3u, { SInt32 })
+    object SInt64Type : NumberType(4u, { SInt64 })
+    object UInt8Type : NumberType(5u, { UInt8 })
+    object UInt16Type : NumberType(6u, { UInt16 })
+    object UInt32Type : NumberType(7u, { UInt32 })
+    object UInt64Type : NumberType(8u, { UInt64 })
+    object Float32Type : NumberType(9u, { Float32 })
+    object Float64Type : NumberType(10u, { Float64 })
 
     companion object : IndexedEnumDefinition<NumberType>(
         enumClass = NumberType::class,

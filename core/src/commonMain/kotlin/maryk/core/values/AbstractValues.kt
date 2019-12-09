4a678d463f6bfbfbd854e0bda181d62deda711c8
Jurriaan Mous <jurmous@jurmo.us>
2019-12-09 14:54:35 +0100

    Add values processor to AbstractValues

--- core/src/commonMain/kotlin/maryk/core/values/AbstractValues.kt
+++ core/src/commonMain/kotlin/maryk/core/values/AbstractValues.kt
@@ -9,11 +9,22 @@ import maryk.core.models.IsNamedDataModel
 import maryk.core.properties.AbstractPropertyDefinitions
 import maryk.core.properties.IsPropertyContext
 import maryk.core.properties.definitions.HasDefaultValueDefinition
+import maryk.core.properties.definitions.IsEmbeddedValuesDefinition
+import maryk.core.properties.definitions.IsListDefinition
+import maryk.core.properties.definitions.IsMapDefinition
+import maryk.core.properties.definitions.IsMultiTypeDefinition
 import maryk.core.properties.definitions.IsPropertyDefinition
+import maryk.core.properties.definitions.IsSetDefinition
 import maryk.core.properties.definitions.IsTransportablePropertyDefinitionType
 import maryk.core.properties.definitions.wrapper.IsDefinitionWrapper
+import maryk.core.properties.enum.TypeEnum
+import maryk.core.properties.references.CanContainListItemReference
+import maryk.core.properties.references.CanContainMapItemReference
+import maryk.core.properties.references.CanContainSetItemReference
+import maryk.core.properties.references.CanHaveComplexChildReference
 import maryk.core.properties.references.IsFuzzyReference
 import maryk.core.properties.references.IsPropertyReference
+import maryk.core.properties.types.TypedValue
 import maryk.core.query.RequestContext
 import maryk.lib.exceptions.ParseException
 
@@ -143,6 +154,88 @@ abstract class AbstractValues<DO : Any, DM : IsDataModel<P>, P : AbstractPropert
     internal fun remove(index: UInt): Any? {
         return (this.values as MutableValueItems).remove(index)?.value
     }
+
+    /**
+     * Method to walk to any value and process it with the reference
+     * Pass [parentReference] to process within context of parent
+     */
+    fun processAllValues(parentReference: IsPropertyReference<*, *, *>? = null, processor: (IsPropertyReference<out Any, IsPropertyDefinition<out Any>, IsValues<*>>, Any) -> Unit) {
+        for ((index, value) in this.values) {
+            this.dataModel.properties[index]?.let { definition ->
+                processDefinitionForProcessor(
+                    definition.definition as IsPropertyDefinition<out Any>,
+                    value,
+                    definition.ref(parentReference),
+                    processor
+                )
+            }
+        }
+    }
+
+    private fun processDefinitionForProcessor(
+        definition: IsPropertyDefinition<out Any>,
+        value: Any,
+        parentReference: IsPropertyReference<*, *, *>?,
+        processor: (IsPropertyReference<out Any, IsPropertyDefinition<out Any>, IsValues<*>>, Any) -> Unit
+    ) {
+        @Suppress("UNCHECKED_CAST")
+        when (definition) {
+            is IsEmbeddedValuesDefinition<*, *, *> -> {
+                (value as AbstractValues<*, *, *>).processAllValues(parentReference, processor)
+            }
+            is IsListDefinition<*, *> ->
+                for ((listIndex, item) in (value as List<Any>).withIndex()) {
+                    val itemRef = definition.itemRef(
+                        listIndex.toUInt(),
+                        parentReference as CanContainListItemReference<*, *, *>
+                    ) as IsPropertyReference<out Any, IsPropertyDefinition<out Any>, IsValues<*>>
+                    processor(itemRef, item)
+                }
+            is IsSetDefinition<*, *> -> {
+                for (item in value as Set<Any>) {
+                    val itemRef = (definition as IsSetDefinition<Any, *>).itemRef(
+                        item,
+                        parentReference as CanContainSetItemReference<*, *, *>
+                    )
+                    processor(itemRef as IsPropertyReference<out Any, IsPropertyDefinition<out Any>, IsValues<*>>, item)
+                }
+            }
+            is IsMapDefinition<out Any, out Any, *> -> {
+                for ((key, item) in value as Map<Any, Any>) {
+                    val itemRef = (definition as IsMapDefinition<Any, out Any, *>).valueRef(
+                        key,
+                        parentReference as CanContainMapItemReference<*, *, *>
+                    ) as IsPropertyReference<out Any, IsPropertyDefinition<out Any>, IsValues<*>>
+
+                    processDefinitionForProcessor(
+                        definition.valueDefinition,
+                        item,
+                        itemRef,
+                        processor
+                    )
+                }
+            }
+            is IsMultiTypeDefinition<out TypeEnum<Any>, out Any, *> -> {
+                val multiType = value as TypedValue<TypeEnum<Any>, Any>
+                val typeReference = (definition as IsMultiTypeDefinition<TypeEnum<Any>, Any, *>).typedValueRef(
+                    multiType.type,
+                    parentReference as CanHaveComplexChildReference<*, *, *, *>
+                )
+                processDefinitionForProcessor(
+                    definition.definition(multiType.type) as IsPropertyDefinition<out Any>,
+                    multiType.value,
+                    typeReference,
+                    processor
+                )
+            }
+            else ->
+                processor(
+                    parentReference as IsPropertyReference<out Any, IsPropertyDefinition<out Any>, IsValues<*>>,
+                    value
+                )
+        }
+    }
+
 }
 
 
--- core/src/commonTest/kotlin/maryk/core/values/ValuesTest.kt
+++ core/src/commonTest/kotlin/maryk/core/values/ValuesTest.kt
@@ -1,11 +1,15 @@
 package maryk.core.values
 
+import maryk.core.properties.references.IsPropertyReference
+import maryk.core.properties.types.TypedValue
 import maryk.lib.time.DateTime
 import maryk.lib.time.Time
 import maryk.test.models.EmbeddedMarykModel
 import maryk.test.models.Option.V2
+import maryk.test.models.SimpleMarykTypeEnum.S1
 import maryk.test.models.TestMarykModel
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.expect
 
 class ValuesTest {
@@ -109,6 +113,55 @@ class ValuesTest {
         }
         """.trimIndent()) {
             values.toJson(pretty = true)
+        }
+    }
+
+    @Test
+    fun processValuesTest() {
+        val values = TestMarykModel(
+            string = "hello world",
+            int = 5,
+            uint = 3u,
+            double = 2.3,
+            dateTime = DateTime(2018, 7, 18),
+            map = mapOf(
+                Time(11, 22, 33) to "eleven",
+                Time(12, 23, 34) to "twelve"
+            ),
+            embeddedValues = EmbeddedMarykModel(
+                value = "test",
+                model = EmbeddedMarykModel(
+                    value = "another test"
+                )
+            ),
+            multi = TypedValue(S1, "s1value"),
+            listOfString = listOf(
+                "v1", "v2", "v3"
+            )
+        )
+
+        val expectedValues = arrayOf<Pair<IsPropertyReference<*, *, *>, Any?>>(
+            TestMarykModel { string::ref } to values { string },
+            TestMarykModel { int::ref } to values { int },
+            TestMarykModel { uint::ref } to values { uint },
+            TestMarykModel { double::ref } to values { double },
+            TestMarykModel { dateTime::ref } to values { dateTime },
+            TestMarykModel { enum::ref } to values { enum },
+            TestMarykModel { map refAt Time(11, 22, 33) } to "eleven",
+            TestMarykModel { map refAt Time(12, 23, 34) } to "twelve",
+            TestMarykModel { embeddedValues { value::ref } } to "test",
+            TestMarykModel { embeddedValues { model { value::ref } } } to "another test",
+            TestMarykModel { multi.refAtType(S1) } to "s1value",
+            TestMarykModel { listOfString refAt 0u } to "v1",
+            TestMarykModel { listOfString refAt 1u } to "v2",
+            TestMarykModel { listOfString refAt 2u } to "v3"
+        )
+
+        var index = 0
+        values.processAllValues { propertyReference, value ->
+            val expected = expectedValues[index++]
+            assertEquals(expected.first, propertyReference)
+            assertEquals(expected.second, value)
         }
     }
 }

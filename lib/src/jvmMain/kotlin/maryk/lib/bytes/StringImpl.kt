d38deb3926b081a39bc900e17da125b87493107b
Jurriaan Mous <jurmous@jurmo.us>
2019-12-30 14:24:32 +0100

    Improve String creation by recycling bytearray

--- lib/src/jvmMain/kotlin/maryk/lib/bytes/StringImpl.kt
+++ lib/src/jvmMain/kotlin/maryk/lib/bytes/StringImpl.kt
@@ -1,13 +1,23 @@
 package maryk.lib.bytes
 
+import maryk.lib.recyclableByteArray
+
 actual fun initString(bytes: ByteArray, offset: Int, length: Int) =
     String(bytes, offset, length)
 
-actual fun initString(length: Int, reader: () -> Byte) = String(
-    ByteArray(length) {
-        reader()
+actual fun initString(length: Int, reader: () -> Byte) =
+    if (length > recyclableByteArray.size) {
+        String(
+            ByteArray(length) {
+                reader()
+            }
+        )
+    } else {
+        for (index in 0 until length) {
+            recyclableByteArray[index] = reader()
+        }
+        String(recyclableByteArray, 0, length)
     }
-)
 
 actual fun codePointAt(string: String, index: Int) = Character.codePointAt(string, index)
 

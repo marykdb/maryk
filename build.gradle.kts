491515c65a70039e1923c2f77a1d0fb0cda66c4e
Jurriaan Mous <jurmous@jurmo.us>
2020-05-08 21:54:51 +0200

    Update coroutines to 1.3.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 buildscript {
     extra["kotlinVersion"] = "1.3.72"
-    extra["coroutinesVersion"] = "1.3.5"
+    extra["coroutinesVersion"] = "1.3.6"
     extra["marykRocksDBVersion"] = "0.6.1"
 
     repositories {

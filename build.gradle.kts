efd9a3fc6e1a2e1fbe0ad63eb437df62cbf07fb3
Jurriaan Mous <jurmous@jurmo.us>
2020-05-08 23:05:39 +0200

    Upgrade Android plugin to 3.6.0 and rocksdb multiplatform to 0.6.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,13 +4,13 @@ repositories {
 }
 
 plugins {
-    id("com.android.library") version "3.5.3" apply false
+    id("com.android.library") version "3.6.0" apply false
 }
 
 buildscript {
     extra["kotlinVersion"] = "1.3.72"
     extra["coroutinesVersion"] = "1.3.6"
-    extra["marykRocksDBVersion"] = "0.6.1"
+    extra["marykRocksDBVersion"] = "0.6.2"
 
     repositories {
         jcenter()

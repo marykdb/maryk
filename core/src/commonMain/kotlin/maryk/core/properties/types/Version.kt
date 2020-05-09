90f93e3cac3b90e78646d76ff11ffcf022570e18
Jurriaan Mous <jurmous@jurmo.us>
2020-05-09 14:01:24 +0200

    Add semantic version property

---
+++ core/src/commonMain/kotlin/maryk/core/properties/types/Version.kt
@@ -0,0 +1,81 @@
+package maryk.core.properties.types
+
+import maryk.core.models.ValueDataModel
+import maryk.core.properties.IsPropertyContext
+import maryk.core.properties.ObjectPropertyDefinitions
+import maryk.core.properties.definitions.number
+import maryk.core.properties.types.numeric.UInt16
+import maryk.core.values.MutableValueItems
+import maryk.core.values.ObjectValues
+import maryk.core.values.ValueItem
+import maryk.json.IsJsonLikeReader
+import maryk.json.IsJsonLikeWriter
+import maryk.json.JsonToken.StartDocument
+import maryk.json.JsonToken.Value
+import maryk.lib.exceptions.ParseException
+
+private val versionRegEx = Regex("^([0-9]+)[.]([0-9]+)([.]([0-9]+))?$")
+
+/**
+ * A Version according to semantic versioning.
+ * The internal values are UShort so it means that each part has a max 65535
+ */
+data class Version(
+    val major: UShort,
+    val minor: UShort,
+    val patch: UShort
+): ValueDataObject(toBytes(major, minor, patch)) {
+    constructor(major: Int = 1, minor: Int = 0, patch: Int = 0) : this(major.toUShort(), minor.toUShort(), patch.toUShort())
+
+    @Suppress("unused")
+    object Properties : ObjectPropertyDefinitions<Version>() {
+        val major by number(1u, Version::major, type = UInt16)
+        val minor by number(2u, Version::minor, type = UInt16)
+        val patch by number(3u, Version::patch, type = UInt16)
+    }
+
+    companion object : ValueDataModel<Version, Properties>(
+        name = "TestValueObject",
+        properties = Properties
+    ) {
+        override fun invoke(values: ObjectValues<Version, Properties>) = Version(
+            major = values<UShort>(1u),
+            minor = values(2u),
+            patch = values(3u)
+        )
+
+        override fun writeJson(obj: Version, writer: IsJsonLikeWriter, context: IsPropertyContext?) {
+            val patch = if(obj.patch != UShort.MIN_VALUE) {".${obj.patch}" } else ""
+            writer.writeString("${obj.major}.${obj.minor}$patch")
+        }
+
+        override fun readJsonToMap(reader: IsJsonLikeReader, context: IsPropertyContext?): MutableValueItems {
+            if (reader.currentToken == StartDocument) {
+                reader.nextToken()
+            }
+
+            return when (val token = reader.currentToken) {
+                is Value<*> -> {
+                    val value = token.value.toString()
+
+                    val (major, minor, _, patch) = versionRegEx.matchEntire(value)?.destructured
+                        ?: throw ParseException("Invalid version: ${value}")
+
+                    MutableValueItems().also { items ->
+                        try {
+                            items += ValueItem(Properties.major.index, major.toUShort())
+                            items += ValueItem(Properties.minor.index, minor.toUShort())
+                            val realPatch = if (patch.isNotBlank()) patch.toUShort() else 0.toUShort()
+                            items += ValueItem(Properties.patch.index, realPatch)
+                        } catch (e: Throwable) {
+                            throw ParseException("Invalid version: ${value}", e)
+                        }
+                    }
+                }
+                else -> {
+                    super.readJsonToMap(reader, context)
+                }
+            }
+        }
+    }
+}
---
+++ core/src/commonTest/kotlin/maryk/core/properties/types/VersionTest.kt
@@ -0,0 +1,72 @@
+package maryk.core.properties.types
+
+import maryk.checkJsonConversion
+import maryk.checkProtoBufConversion
+import maryk.checkYamlConversion
+import maryk.json.InvalidJsonContent
+import maryk.lib.exceptions.ParseException
+import kotlin.test.Test
+import kotlin.test.assertFailsWith
+import kotlin.test.expect
+
+internal class VersionTest {
+    private val version = Version(1, 2, 3)
+
+    @Test
+    fun convertToProtoBufAndBack() {
+        checkProtoBufConversion(this.version, Version)
+    }
+
+    @Test
+    fun convertToJSONAndBack() {
+        expect(
+            """
+            "1.2.3"
+            """.trimIndent()
+        ) {
+            checkJsonConversion(this.version, Version)
+        }
+
+        expect(
+            """
+            "1.2"
+            """.trimIndent()
+        ) {
+            checkJsonConversion(Version(1, 2), Version)
+        }
+    }
+
+    @Test
+    fun convertToYAMLAndBack() {
+        expect(
+            """
+            1.2.3
+            """.trimIndent()
+        ) {
+            checkYamlConversion(this.version, Version)
+        }
+
+        expect(
+            """
+            3.4
+            """.trimIndent()
+        ) {
+            checkYamlConversion(Version(3, 4), Version)
+        }
+    }
+
+    @Test
+    fun failOnInvalidJson() {
+        assertFailsWith<ParseException> {
+            Version.readJson(""""1.2.3.4.5"""")
+        }
+
+        assertFailsWith<ParseException> {
+            Version.readJson(""""Something dirty"""")
+        }
+
+        assertFailsWith<InvalidJsonContent> {
+            Version.readJson("1.2.3.4")
+        }
+    }
+}

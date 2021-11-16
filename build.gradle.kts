465c4cafb298ff5a7db0c2de7408a89e472316e8
Jurriaan Mous <jurmous@jurmo.us>
2021-11-16 16:55:23 +0100

    Update to Kotlin 1.6.0 proper

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 buildscript {
-    extra["kotlinVersion"] = "1.6.0-RC2"
+    extra["kotlinVersion"] = "1.6.0"
     extra["coroutinesVersion"] = "1.5.2"
     extra["marykRocksDBVersion"] = "6.25.3"
 

e5ec84aa5c6472660feba2df44d8701fe7b8bcb5
Jurriaan Mous <jurmous@jurmo.us>
2021-11-22 20:45:05 +0100

    Move initString from platform specific impl to common impl

--- lib/src/jvmMain/kotlin/maryk/lib/bytes/StringImpl.kt
+++ lib/src/jvmMain/kotlin/maryk/lib/bytes/StringImpl.kt
@@ -1,8 +1,5 @@
 package maryk.lib.bytes
 
-actual fun initString(bytes: ByteArray, offset: Int, length: Int) =
-    bytes.decodeToString(offset, offset + length)
-
 actual fun codePointAt(string: String, index: Int) = Character.codePointAt(string, index)
 
 actual fun fromCodePoint(value: Int): String {

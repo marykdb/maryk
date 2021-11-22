60784452cd990bc03f921c9258889e019f5307f6
Jurriaan Mous <jurmous@jurmo.us>
2021-11-22 20:37:23 +0100

    Simplify zeroFill with standard library call.

--- lib/src/commonMain/kotlin/maryk/lib/extensions/zeroFill.kt
+++ lib/src/commonMain/kotlin/maryk/lib/extensions/zeroFill.kt
@@ -1,10 +1,5 @@
 package maryk.lib.extensions
 
 /** Converts number to String and prefixes zeros until [totalDigits] count is reached */
-fun Number.zeroFill(totalDigits: Int): String {
-    var string = this.toString()
-    for (it in string.length until totalDigits) {
-        string = "0$string"
-    }
-    return string
-}
+fun Number.zeroFill(totalDigits: Int) =
+    this.toString().padStart(totalDigits, '0')

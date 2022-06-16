0d128acaa2464612271600f28b4babfa073239a2
Jurriaan Mous <jurmous@jurmo.us>
2022-06-16 15:26:24 +0200

    Update to Kotlin 1.7.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 buildscript {
-    extra["kotlinVersion"] = "1.7.0-RC2"
+    extra["kotlinVersion"] = "1.7.0"
     extra["coroutinesVersion"] = "1.5.2"
     extra["marykRocksDBVersion"] = "7.0.3"
 

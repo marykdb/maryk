961bba7a55029fdec04c3ba6a96b5534959d6709
Jurriaan Mous <jurmous@jurmo.us>
2022-07-13 22:34:42 +0200

    Remove unused zeroFill method

--- lib/src/commonMain/kotlin/maryk/lib/extensions/zeroFill.kt
+++
@@ -1,5 +0,0 @@
-package maryk.lib.extensions
-
-/** Converts number to String and prefixes zeros until [totalDigits] count is reached */
-fun Number.zeroFill(totalDigits: Int) =
-    this.toString().padStart(totalDigits, '0')

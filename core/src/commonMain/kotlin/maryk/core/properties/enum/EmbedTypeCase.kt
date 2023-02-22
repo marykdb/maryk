d3fac68312a556d8b295b38a4ba9687461493661
Jurriaan Mous <jurmous@jurmo.us>
2023-02-22 23:11:00 +0100

    Remove unused interface EmbedTypeCase

--- core/src/commonMain/kotlin/maryk/core/properties/enum/EmbedTypeCase.kt
+++
@@ -1,6 +0,0 @@
-package maryk.core.properties.enum
-
-import maryk.core.properties.PropertyDefinitions
-
-/** Defines an enum case for an embed so types can be strongly resolved */
-interface EmbedTypeCase<E: IndexedEnum, P: PropertyDefinitions>

73aa9e9141d6630ded1dcf644ae9022c5f68fa36
Jurriaan Mous <jurmous@jurmo.us>
2023-03-25 22:43:06 +0100

    Convert IndexUpdateType to use the new model internally

--- core/src/commonMain/kotlin/maryk/core/query/changes/IndexUpdateType.kt
+++ core/src/commonMain/kotlin/maryk/core/query/changes/IndexUpdateType.kt
@@ -1,6 +1,7 @@
 package maryk.core.query.changes
 
 import maryk.core.models.IsObjectDataModel
+import maryk.core.properties.IsObjectPropertyDefinitions
 import maryk.core.properties.IsSimpleBaseModel
 import maryk.core.properties.definitions.EmbeddedObjectDefinition
 import maryk.core.properties.enum.IndexedEnumComparable
@@ -12,16 +13,16 @@ import maryk.core.query.RequestContext
 /** Indexed type of index update */
 enum class IndexUpdateType(
     override val index: UInt,
-    val dataModel: IsObjectDataModel<out IsIndexUpdate, *>
+    val dataModel: IsObjectPropertyDefinitions<out IsIndexUpdate>
 ) : IndexedEnumComparable<IndexUpdateType>,
     IsCoreEnum,
     MultiTypeEnum<IsIndexUpdate> {
-    Update(1u, IndexUpdate.Model),
-    Delete(2u, IndexDelete.Model);
+    Update(1u, IndexUpdate),
+    Delete(2u, IndexDelete);
 
     @Suppress("UNCHECKED_CAST")
     override val definition = EmbeddedObjectDefinition(
-        dataModel = { dataModel.properties as IsSimpleBaseModel<IsIndexUpdate, RequestContext, RequestContext> }
+        dataModel = { dataModel as IsSimpleBaseModel<IsIndexUpdate, RequestContext, RequestContext> }
     )
 
     override val alternativeNames: Set<String>? = null

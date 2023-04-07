ce4178906354819c391d806fd91fcc17bd4d3b69
Jurriaan Mous <jurmous@jurmo.us>
2023-04-07 14:56:49 +0200

    Remove property reference getting functions from IsNamedDataModelDefinition

--- core/src/commonMain/kotlin/maryk/core/models/definitions/IsNamedDataModelDefinition.kt
+++ core/src/commonMain/kotlin/maryk/core/models/definitions/IsNamedDataModelDefinition.kt
@@ -1,30 +1,7 @@
 package maryk.core.models.definitions
 
-import maryk.core.exceptions.DefNotFoundException
 import maryk.core.models.IsDataModel
-import maryk.core.properties.IsPropertyContext
 
 interface IsNamedDataModelDefinition<DM : IsDataModel> : IsDataModelDefinition<DM> {
     val name: String
-
-    /** Get PropertyReference by [referenceName] */
-    fun getPropertyReferenceByName(referenceName: String, context: IsPropertyContext? = null) = try {
-        this.properties.getPropertyReferenceByName(referenceName, context)
-    } catch (e: DefNotFoundException) {
-        throw DefNotFoundException("Model ${this.name}: ${e.message}")
-    }
-
-    /** Get PropertyReference by bytes by reading the [reader] until [length] is reached. */
-    fun getPropertyReferenceByBytes(length: Int, reader: () -> Byte, context: IsPropertyContext? = null) = try {
-        this.properties.getPropertyReferenceByBytes(length, reader, context)
-    } catch (e: DefNotFoundException) {
-        throw DefNotFoundException("Model ${this.name}: ${e.message}")
-    }
-
-    /** Get PropertyReference by bytes by reading the storage bytes [reader] until [length] is reached. */
-    fun getPropertyReferenceByStorageBytes(length: Int, reader: () -> Byte, context: IsPropertyContext? = null) = try {
-        this.properties.getPropertyReferenceByStorageBytes(length, reader, context)
-    } catch (e: DefNotFoundException) {
-        throw DefNotFoundException("Model ${this.name}: ${e.message}")
-    }
 }

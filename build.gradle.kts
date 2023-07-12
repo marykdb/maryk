aec5162472ae902c201811e17143186698d5d409
Adam <897017+aSemy@users.noreply.github.com>
2023-07-12 10:45:17 +0200

    update to Kotlin 1.9 and use newer K/N feature flags to try and fix dealloc issue https://github.com/marykdb/maryk/pull/5#issuecomment-1632011124

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,8 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompilerOptions
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
+
 repositories {
     mavenCentral()
     google()
@@ -16,13 +21,20 @@ buildscript {
     }
 }
 allprojects {
-    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().all {
-        kotlinOptions {
-            languageVersion = "1.8"
-            apiVersion = "1.8"
-            freeCompilerArgs += "-progressive"
-            allWarningsAsErrors = true
-            jvmTarget = "1.8"
+    // TODO update to use convention plugins when https://github.com/marykdb/maryk/pull/2 is merged
+    tasks.withType<KotlinCompilationTask<*>>().configureEach {
+        compilerOptions {
+            languageVersion.set(KotlinVersion.KOTLIN_1_9)
+            apiVersion.set(languageVersion)
+            freeCompilerArgs.addAll(
+                "-progressive",
+                "-Xallocator=custom", // https://kotlinlang.org/docs/whatsnew19.html#preview-of-custom-memory-allocator
+                "-opt-in=kotlinx.cinterop.ExperimentalForeignApi"
+            )
+//            allWarningsAsErrors.set(true)
+            if (this is KotlinJvmCompilerOptions) {
+                jvmTarget.set(JvmTarget.JVM_1_8)
+            }
         }
     }
     repositories {
--- lib/src/darwinMain/kotlin/maryk/lib/RecyclableByteArrays.kt
+++ lib/src/darwinMain/kotlin/maryk/lib/RecyclableByteArrays.kt
@@ -1,5 +1,7 @@
 package maryk.lib
 
+import kotlin.native.concurrent.ThreadLocal
+
 @ThreadLocal
 actual val recyclableByteArray = ByteArray(10000)
 

710f365172b197027818f6b09de8105029d4a7a8
Jurriaan Mous <jurmous@jurmo.us>
2023-12-03 14:49:34 +0100

    Update to Kotlin 1.9.21 and new Hierarchical structure for native

--- build-logic/src/main/kotlin/maryk/conventions/kotlin-multiplatform-native.gradle.kts
+++ build-logic/src/main/kotlin/maryk/conventions/kotlin-multiplatform-native.gradle.kts
@@ -1,8 +1,5 @@
 package maryk.conventions
 
-import org.gradle.kotlin.dsl.creating
-import org.gradle.kotlin.dsl.getting
-
 /** conventions for a Kotlin/Native subproject */
 
 plugins {
@@ -42,7 +39,9 @@ kotlin {
 
     //mingwX64() // not supported by io.maryk.rocksdb:rocksdb-multiplatform
 
-    ios()     // shortcut that includes: iosArm64, iosX64
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
     macosArm64()
     macosX64()
 
@@ -57,37 +56,4 @@ kotlin {
     //androidNativeX86()
     //androidNativeX64()
     //watchosDeviceArm64()
-
-    @Suppress("UNUSED_VARIABLE")
-    sourceSets {
-        val commonMain by getting {}
-        val commonTest by getting {}
-
-        val nativeMain by creating { dependsOn(commonMain) }
-        val nativeTest by creating { dependsOn(commonTest) }
-
-        // Linux
-        //val linuxX64Main by getting { dependsOn(nativeMain) }
-        //val linuxX64Test by getting { dependsOn(nativeTest) }
-        //val linuxArm64Main by getting { dependsOn(nativeMain) }
-        //val linuxArm64Test by getting { dependsOn(nativeTest) }
-
-        // Windows - MinGW
-        //val mingwX64Main by getting { dependsOn(nativeMain) }
-        //val mingwX64Test by getting { dependsOn(nativeTest) }
-
-        // Apple - macOS
-        val darwinMain by creating { dependsOn(nativeMain) }
-        val darwinTest by creating { dependsOn(nativeTest) }
-
-        val iosArm64Main by getting { dependsOn(darwinMain) }
-        val iosArm64Test by getting { dependsOn(darwinTest) }
-        val iosX64Main by getting { dependsOn(darwinMain) }
-        val iosX64Test by getting { dependsOn(darwinTest) }
-
-        val macosArm64Main by getting { dependsOn(darwinMain) }
-        val macosArm64Test by getting { dependsOn(darwinTest) }
-        val macosX64Main by getting { dependsOn(darwinMain) }
-        val macosX64Test by getting { dependsOn(darwinTest) }
-    }
 }

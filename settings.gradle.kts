c5e8f843f5d034f2d952de5aecf19c26079c5cee
Jurriaan Mous <jurmous@jurmo.us>
2024-05-14 23:07:55 +0200

    Update dependency versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -78,7 +78,7 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
 
 plugins {
-    id("de.fayard.refreshVersions") version "0.60.3"
+    id("de.fayard.refreshVersions") version "0.60.5"
 }
 
 @Suppress("UnstableApiUsage")

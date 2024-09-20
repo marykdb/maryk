46f4f921216db2a729e8315fdadd0d42e97fa438
Jurriaan Mous <jurmous@jurmo.us>
2024-09-20 23:16:48 +0200

    Improve synchronizedIteration function

--- lib/src/commonMain/kotlin/maryk/lib/synchronizedIteration.kt
+++ lib/src/commonMain/kotlin/maryk/lib/synchronizedIteration.kt
@@ -13,43 +13,25 @@ fun <T> synchronizedIteration(
     processOnlyOnIterator1: (T) -> Unit = {},
     processOnlyOnIterator2: (T) -> Unit = {}
 ) {
-    var value1: T? = iterator1.next()
-    var value2: T? = iterator2.next()
+    if (!iterator1.hasNext() && !iterator2.hasNext()) return
 
-    fun process1(value: T) {
-        processOnlyOnIterator1(value)
-        value1 = if (iterator1.hasNext()) iterator1.next() else null
-    }
+    var value1: T? = if (iterator1.hasNext()) iterator1.next() else null
+    var value2: T? = if (iterator2.hasNext()) iterator2.next() else null
 
-    fun process2(value: T) {
-        processOnlyOnIterator2(value)
-        value2 = if (iterator2.hasNext()) iterator2.next() else null
-    }
-
-    // Walk all stored and new properties of both data models and process them
-    // depending on if they are present only in stored, new or both.
     while (value1 != null || value2 != null) {
-        val val1 = value1
-        val val2 = value2
-
         when {
-            val1 == null ->
-                process2(val2!!)
-            val2 == null ->
-                process1(val1)
-            else -> {
-                val result = comparator.compare(val1, val2)
-                when {
-                    result == 0 -> {
-                        processBoth(val1, val2)
-                        value1 = if (iterator1.hasNext()) iterator1.next() else null
-                        value2 = if (iterator2.hasNext()) iterator2.next() else null
-                    }
-                    result > 0 ->
-                        if (iterator1.hasNext()) process1(val1) else process2(val2)
-                    result < 0 ->
-                        if (iterator2.hasNext()) process2(val2) else process1(val1)
+            value2 == null || (value1 != null && comparator.compare(value1, value2) <= 0) -> {
+                if (value2 != null && comparator.compare(value1!!, value2) == 0) {
+                    processBoth(value1, value2)
+                    value2 = if (iterator2.hasNext()) iterator2.next() else null
+                } else {
+                    processOnlyOnIterator1(value1!!)
                 }
+                value1 = if (iterator1.hasNext()) iterator1.next() else null
+            }
+            else -> {
+                processOnlyOnIterator2(value2)
+                value2 = if (iterator2.hasNext()) iterator2.next() else null
             }
         }
     }

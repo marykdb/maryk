cf1c836b06582b09ba6fcf0da67057c4c6a18863
Jurriaan Mous <jurmous@jurmo.us>
2025-09-04 10:52:04 +0200

    Improve non historic parsing readability in FoundationDB.

--- store/foundationdb/src/commonMain/kotlin/maryk/datastore/foundationdb/processors/helpers/nonHistoricQualifierRetriever.kt
+++ store/foundationdb/src/commonMain/kotlin/maryk/datastore/foundationdb/processors/helpers/nonHistoricQualifierRetriever.kt
@@ -3,15 +3,13 @@ package maryk.datastore.foundationdb.processors.helpers
 internal fun FDBIterator.nonHistoricQualifierRetriever(
     prefix: ByteArray
 ): (((Int) -> Byte, Int) -> Unit) -> Boolean = { resultHandler ->
-    if (this.hasNext()) {
+    var emitted = false
+    while (this.hasNext()) {
         val value = next()
-
         val qualifier = value.key
-
         resultHandler({ qualifier[prefix.size + it] }, qualifier.size - prefix.size)
-
-        true
-    } else {
-        false
+        emitted = true
+        break
     }
+    emitted
 }

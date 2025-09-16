87cd039c71fd4e79b6b053db2a7ca9b171084e24
Jurriaan Mous <jurmous@jurmo.us>
2025-09-16 18:36:14 +0200

    Add WASM support

--- build-logic/src/main/kotlin/maryk/conventions/kotlin-multiplatform-js.gradle.kts
+++ build-logic/src/main/kotlin/maryk/conventions/kotlin-multiplatform-js.gradle.kts
@@ -1,14 +1,22 @@
 package maryk.conventions
 
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+
 /** conventions for a Kotlin/JS subproject */
 
 plugins {
     id("maryk.conventions.kotlin-multiplatform-base")
 }
 
+@OptIn(ExperimentalWasmDsl::class)
 kotlin {
     js {
         browser()
         nodejs()
+    }
+    wasmJs {
+        browser()
+        nodejs()
+        binaries.library()
     }
 }
